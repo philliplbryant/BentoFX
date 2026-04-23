f1902a036e2ffdebc38c7d95523c11b89d4d90ad
Phil Bryant <phillip.l.bryant@saic.com>
2026-04-23 13:37:15 -0500

    BENTO-31: * Remove Jacoco plugin.

--- build-logic/src/main/groovy/software/coley/gradle/lifecycle/TestLifecycle.groovy
+++ build-logic/src/main/groovy/software/coley/gradle/lifecycle/TestLifecycle.groovy
@@ -1,43 +1,10 @@
 package software.coley.gradle.lifecycle
 
-import org.gradle.api.provider.Provider
-import org.gradle.api.provider.ProviderFactory
-
-import static software.coley.gradle.lifecycle.TestLifecycle.TestReportMode.ALL
-import static software.coley.gradle.lifecycle.TestLifecycle.TestReportMode.CI
-import static software.coley.gradle.lifecycle.TestLifecycle.TestReportMode.DEV
-import static software.coley.gradle.lifecycle.TestLifecycle.TestReportMode.OFF
-
 final class TestLifecycle {
+
 	public static final String CHECK_INTEGRATION_TASK_NAME = 'checkIntegration'
 	public static final String CHECK_FUNCTIONAL_TASK_NAME = 'checkFunctional'
 	public static final String CHECK_ALL_TASK_NAME = 'checkAll'
-
-	static Provider<Boolean> enableJacoco(ProviderFactory providers) {
-		return providers.gradleProperty('collectCoverage')
-				.map(it ->
-                        it != 'false'
-				).orElse(false)
-	}
-
-	static Provider<TestReportMode> getTestReportMode(ProviderFactory providers) {
-		return providers.gradleProperty('testReportMode')
-				.map(it ->
-						switch (it.toLowerCase()) {
-							case 'off' -> OFF
-							case 'all' -> ALL
-							case 'ci' -> CI
-							default -> DEV
-						}
-				).orElse(DEV)
-	}
-
-	enum TestReportMode {
-		OFF,
-		ALL,
-		CI,
-		DEV
-	}
 
 	private TestLifecycle() {
 		throw new UnsupportedOperationException(
