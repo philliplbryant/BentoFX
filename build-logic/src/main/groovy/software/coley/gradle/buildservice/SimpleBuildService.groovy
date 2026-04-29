e5706f73af6dc4bba92579d6dc642c0eb51eda33
Phil Bryant <phillip.l.bryant@saic.com>
2026-04-29 10:55:00 -0500

    BENTO-35: * Remove unused convention scripts.

--- build-logic/src/main/groovy/software/coley/gradle/buildservice/SimpleBuildService.groovy
+++
@@ -1,7 +0,0 @@
-package software.coley.gradle.buildservice
-
-import org.gradle.api.services.BuildService
-import org.gradle.api.services.BuildServiceParameters
-
-abstract class SimpleBuildService implements BuildService<BuildServiceParameters.None> {
-}
