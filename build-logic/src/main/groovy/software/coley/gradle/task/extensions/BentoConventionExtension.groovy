e19aa0596ae37d55eb0d50e7f5a9a07a483a7719
Phil Bryant <phillip.l.bryant@saic.com>
2026-05-15 08:14:37 -0500

    BENTO-35: * Add documentation to the `BentoConventionExtension`. * Remove trailing whitespaces from values in the publish convention.

--- build-logic/src/main/groovy/software/coley/gradle/task/extensions/BentoConventionExtension.groovy
+++ build-logic/src/main/groovy/software/coley/gradle/task/extensions/BentoConventionExtension.groovy
@@ -4,9 +4,22 @@ import org.gradle.api.provider.Provider
 
 import javax.inject.Inject
 
+/**
+ * Extension class for sharing lazily calculated values among precompiled
+ * convention script plugins.
+ */
 abstract class BentoConventionExtension {
+
+    /**
+     * The fully qualified project name, normalized as a valid for use as an
+     * artifact ID, JAR file name, etc.
+     */
     final Provider<String> normalizedProjectName
 
+    /**
+     * @param normalizedProjectName The fully qualified project name,
+     * normalized as a valid for use as an artifact ID, JAR file name, etc.
+     */
     @Inject
     BentoConventionExtension(final Provider<String> normalizedProjectName) {
         this.normalizedProjectName = normalizedProjectName
