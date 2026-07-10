78b142cea63e6ea7c022bb8f98388e8a7d05efd9
philliplbryant <philliplbryant@gmail.com>
2026-07-10 10:22:27 -0500

    BENTO-13: * Move DAGP settings configuration from root `settings.gradle` to `DependencyAnalysisSettingsPlugin`.

--- settings-logic/src/main/groovy/software/coley/gradle/plugin/analysis/DependencyAnalysisSettingsPlugin.groovy
+++ settings-logic/src/main/groovy/software/coley/gradle/plugin/analysis/DependencyAnalysisSettingsPlugin.groovy
@@ -1,5 +1,6 @@
 package software.coley.gradle.plugin.analysis
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
@@ -13,5 +14,18 @@ class DependencyAnalysisSettingsPlugin implements Plugin<Settings> {
     @Override
     void apply(final Settings settings) {
         settings.pluginManager.apply('com.autonomousapps.build-health')
+        configureDependencyAnalysis(settings)
+    }
+
+    @CompileDynamic
+    private static void configureDependencyAnalysis(final Settings settings) {
+        settings.extensions.configure('dependencyAnalysis') { extension ->
+            extension.usage {
+                analysis {
+                    checkSuperClasses(true)
+                }
+            }
+            extension.useTypesafeProjectAccessors(true)
+        }
     }
 }
