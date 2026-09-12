420a8022b2d0130a80f9ffe22d3f5109c912c021
Phil Bryant <phillip.l.bryant@saic.com>
2026-09-12 11:10:15 -0500

    master13: * Add imports missing from earlier sync with Col-E.

--- core/src/main/java/software/coley/bentofx/control/Header.java
+++ core/src/main/java/software/coley/bentofx/control/Header.java
@@ -27,6 +27,8 @@ import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.jspecify.annotations.Nullable;
